8e378f75296f3e4cc8dd57fd9cd4e4445b59371f
Maxim <paranin.maxim@mail.ru>
2025-01-31 14:33:04 +0300

    Добавлена проверка на латиницу для английских слов, хендлеры обернуты в try-except, проверки на пустой словарь

--- src/handlers.py
+++ src/handlers.py
@@ -1,12 +1,14 @@
 from aiogram.filters.command import Command
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+
 
 from src import requests as rq
 from src import keyboards as kb
 
 router = Router()
+
 
 
 class AddWord(StatesGroup):
@@ -20,43 +22,57 @@ class CheckMyKnowledge(StatesGroup):
 
 @router.message(Command('start'))
 async def cmd_start(message: types.Message):
-    await rq.set_user(message.from_user.id) 
-    await message.answer('Привет, я твой личный словарь английских слов. Ты можешь добавить слово в словарь, а потом проверить свои знания!', reply_markup=kb.main)
-
+    try:
+        await rq.set_user(message.from_user.id) 
+        await message.answer('Привет, я твой личный словарь английских слов. Ты можешь добавить слово в словарь, а потом проверить свои знания!', reply_markup=kb.main)
+    except Exception as e:
+        await message.answer(f'Произошла ошибка: {e}')
 
 @router.message(Command('help'))
 async def cmd_help(message: types.Message):
     await message.answer('Чтобы добавить слово в словарь, напиши /add. Чтобы проверить свои знания, напиши /check')
 
 
-@router.message(Command('add'))
+@router.message(F.text == 'Добавить слово')
 async def cmd_add_word_first(message: types.Message, state: FSMContext):
-    await state.set_state(AddWord.word)
-    await message.answer('Впишите слово на английском которое хотите добавить')
+    try:
+        await state.set_state(AddWord.word)
+        await message.answer('Впишите слово на английском которое хотите добавить')
+    except Exception as e:
+        await message.answer(f'Произошла ошибка: {e}')
 
 
 @router.message(AddWord.word)
 async def cmd_add_word_second(message: types.Message, state: FSMContext):
-    await state.update_data(word=message.text)
-    await state.set_state(AddWord.translate)
-    await message.answer('Впишите перевод слова')
+    try:
+        if await rq.check_is_latin(message.text.lower()):
+            await state.update_data(word=message.text.lower())
+            await state.set_state(AddWord.translate)
+            await message.answer('Впишите перевод слова')
+        else:
+            await message.answer('Слово должно состоять только из латинских букв')
+            await state.clear()
+    except Exception as e:
+        await state.clear()
+        await message.answer(f'Произошла ошибка {e}')
 
 
 @router.message(AddWord.translate)
 async def cmd_add_word_third(message: types.Message, state: FSMContext):
-    await state.update_data(translate=message.text)
-    data = await state.get_data()
     try:
+        await state.update_data(translate=message.text.lower())
+        data = await state.get_data()
         if await rq.add_word_translate(message.from_user.id, data['word'], data['translate']):
             await message.answer(f'Слово {data["word"]} уже есть в словаре')
         else:
             await message.answer(f'Слово {data["word"]} успешно добавлено в словарь')
     except Exception as e:
         await message.answer(f'Произошла ошибка: {e}')
-    await state.clear()
+    finally:
+        await state.clear()
 
 
-@router.message(Command('cmk'))
+@router.message(F.text == 'Проверить свои знания')
 async def cmd_check_my_knowledge(message: types.Message, state: FSMContext):
     try:
         await state.clear()
@@ -73,11 +89,15 @@ async def cmd_check_my_knowledge(message: types.Message, state: FSMContext):
 
 @router.message(CheckMyKnowledge.waiting_translate_word)
 async def cmd_check_my_knowledge_second(message: types.Message, state: FSMContext):
+    try:
+        await state.update_data(waiting_translate_word=message.text)
+        data = await state.get_data()
+        if data['translate'] != data['waiting_translate_word']:
+            await message.answer(f'К сожалению, вы ошиблись. Правильный перевод слова {data["word"]} будет {data["translate"]}')
+        else:
+            await message.answer('Вы перевели правильно')
+    except Exception as e:
+        await message.answer(f'Произошла ошибка: {e}')
+    finally:
+        await state.clear()
 
-    await state.update_data(waiting_translate_word=message.text)
-    data = await state.get_data()
-    if data['translate'] != data['waiting_translate_word']:
-        await message.answer(f'К сожалению, вы ошиблись. Правильный перевод слова {data["word"]} будет {data["translate"]}')
-    else:
-        await message.answer('Вы перевели правильно')
-    await state.clear()
--- src/requests.py
+++ src/requests.py
@@ -1,11 +1,12 @@
-from random import randint
 from sqlalchemy import select, func
+from string import ascii_lowercase
 
 from .models import async_session
 from .models import User, Word
 
+DEFAULT_LIMIT_WORDLIST = 100
 
-async def set_user(tg_id):
+async def set_user(tg_id: int):
     '''Добавляет пользователя в базу данных'''
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
@@ -15,8 +16,8 @@ async def set_user(tg_id):
             await session.commit()
 
 
-async def add_word_translate(tg_id, word, translate):
-    '''Добавляет слово в словарь'''
+async def add_word_translate(tg_id: int, word: str, translate: str):
+    '''Проверяет существует ли слово в словаре и добавляет его в словарь'''
     async with async_session() as session:
         if await session.scalar(select(Word).where(Word.word==word, Word.user_id==tg_id)):
             return True
@@ -25,17 +26,21 @@ async def add_word_translate(tg_id, word, translate):
             await session.commit()
 
 
-async def get_random_word(tg_id):
+async def get_random_word(tg_id: int):
     '''Возвращает случайное слово из словаря'''
     async with async_session() as session:
 
-        try:
-            word = await session.scalar(
-                select(Word).where(Word.user_id == tg_id).order_by(func.random()).limit(1)
-            )
-            if word:
-                return word
-            else:
-                return None
-        except Exception as e:
-            return e
+        word = await session.scalar(
+            select(Word).where(Word.user_id == tg_id).order_by(func.random()).limit(1)
+        )
+        if word:
+            return word
+        else:
+            return None
+
+
+async def check_is_latin(input_word: str):
+    '''Проверяет все ли символы слова являются латинскими буквами'''
+    return all(char in ascii_lowercase for char in input_word)
+
+
